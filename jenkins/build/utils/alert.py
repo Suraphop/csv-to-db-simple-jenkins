2e62912cf8385bf5e63108ec614ed673a69898bd
Suraphop <62558689+Suraphop@users.noreply.github.com>
2023-07-06 11:16:41 +0700

    Update alert.py add slack alert

--- jenkins/build/utils/alert.py
+++ jenkins/build/utils/alert.py
@@ -9,7 +9,19 @@ def line_notify(token,msg):
     except Exception as e:
         return e
 
+def slack_notify(webhook_url,msg):
+    try:
+        payload = '{"text":"%s"}' %msg
+        r = requests.post(webhook_url,data=payload)
+        return r.text
+    except Exception as e:
+        return e
+        
 if __name__ == "__main__":
-    import constant
+    print("must be run with main")
+    
     value = line_notify("yixwt2Mpe0I4v7WWWrLV2VXmzgTg1e3AWa6RFj6YhQB","hello")
+    print(value)
+  
+    value = slack_notify("https://hooks.slack.com/services/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","testing_token") 
     print(value)
